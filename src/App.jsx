270ece493dfd5856495964a8fcce76eb233df5bf
y inoue <admin@wainoMacBook-Pro.local>
2021-10-12 22:17:00 +0900

    add-todo-functionally

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,25 @@ export default function App () {
     const [ todos, setTodos ] = useState([])
     const [ todo, setTodo ] = useState("")
 
+    function handleInputChange(e){
+        setTodo(e.target.value)
+    }
+
+    function handleFormSubmit(e){
+        e.preventDefault()
+
+        if (todo !=="") {
+            setTodos([
+                ...todos,
+                {
+                    id: todos.length + 1,
+                    text: todo.trim()
+                }
+            ])
+        }
+        setTodo("")
+    }
+
     return (
         <>
             <form>
